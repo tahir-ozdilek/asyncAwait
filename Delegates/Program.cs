d8b4d1ddd01081b4af65a80e332dc4ead5345adc
Tahir Özdilek <tahir.ozdilek@yilport.com>
2024-01-31 13:30:05 +0300

    anonymous update

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,6 +4,8 @@
     {
         public delegate int MyDelegate(int a, int b);
 
+        public delegate void MyDelegate2(int a, int b);
+
         public static int delegateFunction(int a, int b)
         {
             Console.WriteLine("delegateFunction " + (a + b));
@@ -27,20 +29,25 @@
         static void Main(string[] args)
         {
             MyDelegate myDelegate = delegateFunction;
-            myDelegate(4, 5);
+            myDelegate(1, 2);
+            
+            actionDelegate(2, 2); //actionDelegate.Invoke(2, 2);
+            funcDelegate(2, 3); //funcDelegate.Invoke(2, 3);
 
-            actionDelegate(1, 2); //actionDelegate.Invoke(1, 2);
-            funcFunction(2, 3);
-
+            //A delegate referencing to a anonymous method
+            MyDelegate2 myDelegate2 = (a, b) => { Console.WriteLine("inline anonymous:" + (a + b) ); };
+            myDelegate2(3, 3);
 
             methodTakesFuncParameter(actionFunction);
+
+            methodTakesFuncParameter((a,b) => { Console.WriteLine("inline anonymous method is being called: "+ (a+b)); });
         }
 
 
         static void methodTakesFuncParameter(Action<int, int> action)
         {
             Console.WriteLine("action is being called by action parameter.");
-            action.Invoke(4, 5);
+            action.Invoke(4, 3);
         }
     }
 }
