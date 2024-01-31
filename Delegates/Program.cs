16fa483324ba01c28e125c6d015069a9b3fed9be
Tahir Özdilek <tahir.ozdilek@yilport.com>
2024-01-31 14:30:02 +0300

    delegates

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -3,9 +3,7 @@
     public class Program
     {
         public delegate int IntDelegate(int a, int b);
-
-        public delegate void VoidDelegate(int a, int b);
-       
+        public delegate void VoidDelegate(int a, int b);  
         public delegate bool BoolDelegate(int a, int b);
 
         public static int delegateFunction(int a, int b)
@@ -13,6 +11,7 @@
             Console.WriteLine("delegateFunction " + (a + b));
             return a + b;
         }
+
 
         public static Action<int, int> actionDelegate = actionFunction;
         public static Func<int, int, int> funcDelegate = funcFunction;
@@ -33,18 +32,22 @@
             IntDelegate intDelegate = delegateFunction;
             intDelegate(1, 2);
             
-            actionDelegate(2, 2); //actionDelegate.Invoke(2, 2);
-            funcDelegate(2, 3); //funcDelegate.Invoke(2, 3);
-
             //A delegate referencing to a anonymous method
             VoidDelegate voidDelegate = (a, b) => { Console.WriteLine("inline anonymous:" + (a + b) ); };
             voidDelegate(3, 3);
 
             //A delegate referencing to an expression
             BoolDelegate boolDelegate = (a, b) => a == b;
+            Console.WriteLine(boolDelegate.Invoke(9, 8));
+            Console.WriteLine(boolDelegate.Invoke(9, 9));
 
-            Console.WriteLine(boolDelegate.Invoke(9,8));
-            Console.WriteLine(boolDelegate.Invoke(9,9));
+            //Decleration of a delegate by using 'delegate' keyword
+            BoolDelegate boolDelegate2 = delegate (int a, int b) { return a == b; };
+            Console.WriteLine(boolDelegate2.Invoke(9,8));
+            Console.WriteLine(boolDelegate2.Invoke(9,9));
+
+            actionDelegate(2, 2); //actionDelegate.Invoke(2, 2);
+            funcDelegate(2, 3); //funcDelegate.Invoke(2, 3);
 
             methodTakesFuncParameter(actionFunction);
 
